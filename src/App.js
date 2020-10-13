6f1e26f50dcdd04b1ac3f1336368cbd3f28ca071
Ahmad Amam <ahmadfatkhul.amam@gmail.com>
2020-10-13 13:26:03 +0700

    add detail product page basic style

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import PrivateRoot from './component/privateRoute'
 import Home from './pages/home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import Product from './pages/Product'
+import Product from './pages/DetailProduct'
 import CategoryDetail from './pages/CategoryDetail'
 
 //importing page custommer
@@ -31,7 +31,7 @@ class App extends React.Component{
           <Route path="/" component={Home} exact />
           <Route path="/signup" component={Signup} />
           <Route path='/login' render ={(props)=><Login {...props} />} />
-          <Route path="/product" render={()=><Product/>}/>
+          <Route path="/detail/product" render={()=><Product/>}/>
           <Route path="/category/product" render={()=><CategoryDetail/>}/>
           <Route path="/cart" render={()=><Cart/>}/>
           <Route path="/check_out" render={()=><CheckOut/>}/>
