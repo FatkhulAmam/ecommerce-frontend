ee594b6ca32043a98564134a1671fa045800c07a
Ahmad Amam <ahmadfatkhul.amam@gmail.com>
2020-10-16 16:00:12 +0700

    add eslint for styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import PrivateRoot from './component/privateRoute'
 import Home from './pages/home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import Product from './pages/DetailProduct'
+import ProductDetail from './pages/DetailProduct'
 import CategoryDetail from './pages/CategoryDetail'
 
 //importing page custommer
@@ -17,6 +17,7 @@ import CheckOut from './pages/CheckOut'
 import Profile from './pages/Profile'
 
 //importing page seller
+import MyProduct from './pages/MyProduct'
 import Edit from './pages/edit'
 
 class App extends React.Component{
@@ -33,13 +34,16 @@ class App extends React.Component{
           <Route path="/" component={Home} exact />
           <Route path="/signup" component={Signup} />
           <Route path='/login' render ={(props)=><Login {...props} />} />
-          <Route path="/detail/product" render={()=><Product/>}/>
+          <Route path="/detail/product" render={()=><ProductDetail/>}/>
           <Route path="/category/product" render={()=><CategoryDetail/>}/>
           <Route path="/cart" render={()=><Cart/>}/>
           <Route path="/check_out" render={()=><CheckOut/>}/>
           <PrivateRoot path='/profile'>
             <Profile/>
           </PrivateRoot>
+
+          // path for seller
+          <Route path="/my_product" component={MyProduct} />
           <Route path="/edit/:id" component={Edit}/>
         </Switch>
         </BrowserRouter>
---
+++ src/pages/MyProduct.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import {
+    Container,
+    Row, Col,
+    Card, CardBody, CardTitle, CardSubtitle, CardImg,
+    Modal, ModalHeader, ModalBody, ModalFooter, Button
+} from 'reactstrap'
+
+import Navbar from '../component/NavProfileBar'
+
+import bgProduct from '../assets/image/bgProduct.png'
+
+export default class Product extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: {},
+            modalOpen: false,
+            openedProduct:''
+        }
+    }
+
+    async componentDidMount() {
+        const data = await axios.get("http://localhost:8180/product")
+        this.setState({ data })
+    }
+
+    openModal = async (url) => {
+        // axios.get(url)
+        console.log(url)
+    }
+
+    render() {
+        const { data } = this.state
+        return (
+            <>
+                <Navbar />
+                <Container>
+                    <Row>
+                        {Object.keys(data).length && data.data.data.map(item => {
+                            return (
+                                <Col className="mt-4">
+                                    <Card className="card-home shadow-sm">
+                                        <CardImg className='default-img' src={bgProduct} />
+                                        <CardBody>
+                                            <CardTitle><h5>{item.name}</h5></CardTitle>
+                                            <CardSubtitle className="text-danger mb-2">{item.price}</CardSubtitle>
+                                            <CardSubtitle><h6>{item.category_name}</h6></CardSubtitle>
+                                            <Button onClick={()=>this.openModal(item.url)}>detail</Button>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                </Container>
+                <Modal isOpen={this.state.modalOpen}>
+                    <ModalHeader>Detail Product</ModalHeader>
+                    <ModalBody>{this.state.openedProduct}</ModalBody>
+                    <ModalFooter>
+                        <Button onClick={()=>this.setState({modalOpen:false})}>close</Button>
+                    </ModalFooter>
+                </Modal>
+            </>
+        )
+    }
+}
