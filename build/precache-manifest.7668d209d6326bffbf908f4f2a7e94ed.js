32496e692671b34261ecbd7b120ee2653071d454
Ahmad Amam <ahmadfatkhul.amam@gmail.com>
2020-10-19 08:58:10 +0700

    build react app

---
+++ build/precache-manifest.7668d209d6326bffbf908f4f2a7e94ed.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c6bef9c73fb829cada086213b2cae07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c994720dabca0e054f0",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a468b0215842240bf196",
+    "url": "/static/css/main.d0416b92.chunk.css"
+  },
+  {
+    "revision": "3c994720dabca0e054f0",
+    "url": "/static/js/2.729408a9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.729408a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a468b0215842240bf196",
+    "url": "/static/js/main.a5331a2f.chunk.js"
+  },
+  {
+    "revision": "3cc9d6562f64fc197f20",
+    "url": "/static/js/runtime-main.fc30dc24.js"
+  },
+  {
+    "revision": "3a6a1e6549af60f9784e00e3be86f17c",
+    "url": "/static/media/Search.3a6a1e65.svg"
+  },
+  {
+    "revision": "87754cec32e92fb2d38298e0c10456c3",
+    "url": "/static/media/bell.87754cec.svg"
+  },
+  {
+    "revision": "943859181e520be59c6df0eac4e13952",
+    "url": "/static/media/corou1.94385918.png"
+  },
+  {
+    "revision": "542fcee2d13af0b0833edeb2213c376f",
+    "url": "/static/media/corou2.542fcee2.png"
+  },
+  {
+    "revision": "a6c8d9b55c4050f5ac103e0b7c3583de",
+    "url": "/static/media/filter.a6c8d9b5.svg"
+  },
+  {
+    "revision": "b89a085970e95fc39358cd641f5043fe",
+    "url": "/static/media/logo.b89a0859.svg"
+  },
+  {
+    "revision": "d503db415d56e335cc982ebb7c6adfd5",
+    "url": "/static/media/mail.d503db41.svg"
+  },
+  {
+    "revision": "2a2dfc5f49134084908f146fb8e413a0",
+    "url": "/static/media/shoppingCart.2a2dfc5f.svg"
+  }
+]);
