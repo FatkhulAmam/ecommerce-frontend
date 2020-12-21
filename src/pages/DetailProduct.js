b8fbd8bc33c214ce49f2d4218b0113434a32e491
ahmad amam <ahmadfatkhul.amam@gmail.com>
2020-12-21 10:38:20 +0700

    fix styling detail product

--- src/pages/DetailProduct.js
+++ src/pages/DetailProduct.js
@@ -2,9 +2,13 @@ import React, { Component } from "react";
 import { Container, Row, Button, Modal, ModalBody } from "reactstrap";
 import { connect } from "react-redux";
 import Navbar from "../component/NavSearchBar";
-import { Link } from "react-router-dom";
+import NumberFormat from "react-number-format";
 
 import bgDefault from "../assets/image/bgProduct.png";
+import Star from "../assets/image/star.png";
+
+//import component
+import CardProduct from "../component/CardProduct"
 
 // import redux
 import productAction from "../redux/actions/product";
@@ -72,15 +76,44 @@ class CategoryDetail extends Component {
               </div>
             </div>
             <div className="product-detail mt-4 ml-3">
-              <h2>{dataDetail.name}</h2>
-              <p>toko</p>
-              <p>rating</p>
-              <p>harga</p>
-              <h3>{dataDetail.price}</h3>
-              <p>color</p>
+              <h3 className="font-weight-bold">{dataDetail.name}</h3>
+              <p>Toko Serba Punya</p>
+              <Row>
+                <img className="star-detail ml-3" src={Star} />
+                <img className="star-detail" src={Star} />
+                <img className="star-detail" src={Star} />
+                <img className="star-detail" src={Star} />
+                <img className="star-detail mr-1" src={Star} />
+                <text className="star-detail-num">(10)</text>
+              </Row>
+              <h5>Harga</h5>
+              <h4 className="price-tag font-weight-bold">
+                <NumberFormat
+                  value={dataDetail.price}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"Rp. "}
+                />
+              </h4>
+              <h5>color</h5>
+                <Button className="btn-color rounded-circle"/>
               <Row className="ml-2">
-                <h3>size </h3>
-                <h3 className="ml-3">count</h3>
+                <div>
+                  <h5>size </h5>
+                  <Row>
+                    <Button className="btn-plus rounded-circle">+</Button>
+                    <p className="mt-3">1</p>
+                    <Button className="btn-min rounded-circle">-</Button>
+                  </Row>
+                </div>
+                <div>
+                  <h5 className="ml-4">count</h5>
+                  <Row className="ml-4">
+                    <Button className="btn-plus rounded-circle">+</Button>
+                    <p className="mt-3">1</p>
+                    <Button className="btn-min rounded-circle">-</Button>
+                  </Row>
+                </div>
               </Row>
               <Row>
                 <Button className="btn-chat rounded-pill ml-2">chat</Button>
@@ -97,14 +130,15 @@ class CategoryDetail extends Component {
               </Row>
             </div>
           </Row>
-          <h4 className="mt-5">Informasi Product</h4>
-          <h5 className="mt-5">condition</h5>
-          <h5 className="text-danger">New</h5>
-          <h4 className="mt-4">description</h4>
+          <h4 className="mt-5 font-weight-bold">Informasi Product</h4>
+          <h5 className="mt-4 font-weight-bold">condition</h5>
+          <h6 className="text-danger">New</h6>
+          <h5 className="mt-4 font-weight-bold">description</h5>
           <p>{dataDetail.description}</p>
-          <h4>Product Review</h4>
+          <h4 className="mt-4 font-weight-bold">Product Review</h4>
           <hr />
           <h4>You can also like this</h4>
+          <CardProduct />
         </Container>
         <Modal isOpen={this.state.modal}>
           <ModalBody>{this.props.product.message}</ModalBody>
@@ -129,6 +163,7 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = {
   getDetail: productAction.getDetailById,
   addToCart: productAction.addToCart,
+  getCategoryDetail: productAction.getCategoryDetail,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CategoryDetail);
